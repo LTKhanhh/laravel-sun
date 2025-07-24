96f2d4a3d09533972d83a7003463f093ef60dafa
LTKhanhh <letrongkhanh26112004@gmail.com>
2025-07-24 16:01:58 +0700

    chapter5

--- laravel-quicktask/database/factories/UserFactory.php
+++ laravel-quicktask/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -12,9 +13,11 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The name of the factory's corresponding model.
+     *
+     * @var string
      */
-    protected static ?string $password;
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
@@ -23,22 +26,65 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $firstName = $this->faker->firstName();
+        $lastName = $this->faker->lastName();
+        
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $firstName . ' ' . $lastName,
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'date_of_birth' => $this->faker->date('Y-m-d', '2000-01-01'),
+            'gender' => $this->faker->randomElement(['male', 'female', 'other']),
+            'address' => $this->faker->address(),
+            'avatar' => $this->faker->imageUrl(200, 200, 'people'),
+            'password' => Hash::make('123456'), // mật khẩu mặc định là 'password'
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
+    public function male(): static
+    {
+        return $this->state(function (array $attributes) {
+            $firstName = $this->faker->firstNameMale();
+            $lastName = $this->faker->lastName();
+            
+            return [
+                'first_name' => $firstName,
+                'name' => $firstName . ' ' . $lastName,
+                'gender' => 'male',
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the user is female.
+     */
+    public function female(): static
+    {
+        return $this->state(function (array $attributes) {
+            $firstName = $this->faker->firstNameFemale();
+            $lastName = $this->faker->lastName();
+            
+            return [
+                'first_name' => $firstName,
+                'name' => $firstName . ' ' . $lastName,
+                'gender' => 'female',
+            ];
+        });
+    }
+
+
+    /**
+     * Create user with Vietnamese phone format.
+     */
+    public function vietnamesePhone(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'phone' => $this->faker->regexify('0[0-9]{9}'), // Format: 0xxxxxxxxx
         ]);
     }
 }
